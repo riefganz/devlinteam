08e9809785f8f08585500d70fcb274e105f948f2
riefganz <77000545+riefganz@users.noreply.github.com>
2021-01-05 18:15:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <html>
+<head>
 <title>Gada Apa Apa Bor</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+</head>
 <body>
 <img width="100%" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4QJ16CoX5XOOXsA4UjgDzjtPCxVY09zDQ0g&usqp=CAU" />
 </body>
